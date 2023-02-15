e74dfa4a946df56a9baa6bcdb620f87d9e0ade26
widua <mikolaj.widla@gmail.com>
2023-02-15 17:21:50 +0100

    Changed structure of BookManagerTest

--- bookMicroservice/src/test/java/me/widua/bookMicroservice/manager/BookManagerTest.java
+++ bookMicroservice/src/test/java/me/widua/bookMicroservice/manager/BookManagerTest.java
@@ -9,103 +9,49 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = BookMicroserviceApplication.class
 )
-@ExtendWith(SpringExtension.class)
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
 @ActiveProfiles("test")
 @Transactional
 class BookManagerTest {
-    @Autowired
     private BookManager underTest ;
-    @MockBean
+    @Mock
     private BookRepository repository ;
 
-    private List<BookModel> booksDb ;
 
     @BeforeEach
     public void setUp(){
-        booksDb =  Arrays.asList(
-                new BookModel(
-                        "J.K. Rowling",
-                        "Harry Potter and the Philosopher's Stone" ,
-                        "5006001200" ,
-                        "First book of Harry Potter adventures" ,
-                        BookType.PHYSICAL ,
-                        15),
-                new BookModel(
-                        "J.K. Rowling",
-                        "Harry Potter and the Philosopher's Stone" ,
-                        "1001002003" ,
-                        "First book of Harry Potter adventures" ,
-                        BookType.E_BOOK ,
-                        15),
-                new BookModel(
-                        "Dante Alighieri",
-                        "Divine comedy" ,
-                        "9009008500" ,
-                        "Classic of literature" ,
-                        BookType.PHYSICAL ,
-                        15)
-        );
-
-
+        underTest = new BookManager(repository);
     }
 
-
     @Test
-    public void getAllBooks(){
-        //Given
-        Mockito.when(repository.findAll()).thenReturn(booksDb);
+    public void canGetAllBooks(){
         //When
-        ResponseModel response = underTest.getBooks();
-        List<BookModel> books = (List<BookModel>) response.getBody();
-        //Then
-        assertEquals(response.getStatus(),HttpStatus.OK);
-        assertEquals(books.size() , 3);
+        underTest.getBooks();
+        //Ten
+        verify(repository).findAll();
     }
 
-    @Test
-    public void addBook(){
-
-    }
-
-    @Test
-    public void givenNullIsbnBookShouldReturnBadRequest(){
-        //Given
-        BookModel toInsert = new BookModel(
-                "NonValidBookAuthor",
-                "NonValidBookTitle" ,
-                null ,
-                "NonValidBookDescription" ,
-                BookType.PHYSICAL ,
-                1) ;
-        //When
-
-        ResponseModel response = underTest.addBook(toInsert);
-
-        //Then
-
-
-    }
 
 
 }
