afbf6d64b882c1f3cd4ed84b90dd8002b3fe31f5
Mikołaj <46262654+Widua@users.noreply.github.com>
2023-03-07 21:56:21 +0100

    Manager changed to service, service changed to interface, creates an implementation of service (#3)

--- bookMicroservice/src/test/java/me/widua/bookMicroservice/service/BookServiceImplTest.java
+++ bookMicroservice/src/test/java/me/widua/bookMicroservice/service/BookServiceImplTest.java
@@ -1,26 +1,16 @@
-package me.widua.bookMicroservice.manager;
+package me.widua.bookMicroservice.service;
 
 import me.widua.bookMicroservice.BookMicroserviceApplication;
-import me.widua.bookMicroservice.models.BookModel;
-import me.widua.bookMicroservice.models.ResponseModel;
-import me.widua.bookMicroservice.models.types.BookType;
 import me.widua.bookMicroservice.repositories.BookRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -33,15 +23,15 @@ import static org.mockito.Mockito.verify;
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @ActiveProfiles("test")
 @Transactional
-class BookManagerTest {
-    private BookManager underTest ;
+class BookServiceImplTest {
+    private BookService underTest ;
     @Mock
     private BookRepository repository ;
 
 
     @BeforeEach
     public void setUp(){
-        underTest = new BookManager(repository);
+        underTest = new BookServiceImpl(repository);
     }
 
     @Test
